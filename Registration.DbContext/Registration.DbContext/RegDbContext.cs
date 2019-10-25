9c654f0ad5f6bdaabb4dfd512d12c0df75d11aa9
abraxes <bebemalcho@gmail.com>
2019-10-25 15:46:56 +0300

    all in

---
+++ Registration.DbContext/Registration.DbContext/RegDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Registration.Models;
+
+namespace Registration.DBContext
+{
+    class RegDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=SchoolDB;Trusted_Connection=True;")
+        }
+
+
+    }
+}
---
+++ Registration.DbContext/Registration.Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Registration.Models
+{
+    public class User
+    {
+        [Key]
+        public string IdFuckyou { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
